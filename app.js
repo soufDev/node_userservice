58c286753ad779d964ee4a9302f75acbbd50816a
soufiane <ait.akkache.soufiane@gmail.com>
2018-05-17 03:56:46 +0200

    set db use

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import models from './models/index';
+import models from './db/models';
 
 // init app
 const app = express();
@@ -8,7 +8,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', async (req, res) => {
-  const result = await models.user.findAll();
+
+  console.log('params', req.query, 'users', models.User);
+  const result = await models.User.findAll();
   console.log('params', req.query);
   res.send({ message: `HELLO GUYS I AM HERE ${result}` });
 });
