4dc451000aa0a4891276d8beee0310fd13ccfa63
soufiane <ait.akkache.soufiane@gmail.com>
2018-05-20 18:36:26 +0200

    add post router to user

--- test/api/user.test.js
+++ test/api/user.test.js
@@ -6,7 +6,7 @@ import faker from 'faker';
 import app from '../../app';
 
 before(() => {
-  console.log('test ', env);
+  process.env.SERVER_PORT = 3000;
 });
 
 chai.use(chaiHttp);
@@ -18,24 +18,23 @@ Promise.promisifyAll(request);
 const URI_PREFIX = '/api/v1';
 describe('User API', () => {
   it('GET all', async () => {
-    try {
-      console.log(env);
-      const result = await request.get(`${URI_PREFIX}/users`);
-      expect(result.body).to.be.an('array');
-    } catch (e) {
-      expect(e.message).to.be.equal('error');
-    }
+    console.log(env);
+    const result = await request.get(`${URI_PREFIX}/users`);
+    expect(result.body).to.be.an('array');
   });
   it('add user', async () => {
-    try {
-      const user = {
-        firstname: faker.
-      }
-      const result = await request.post(`${URI_PREFIX}/users`);
-
-    } catch (e) {
-
+    const user = {
+      firstname: faker.name.firstName(),
+      lastname: faker.name.lastName(),
+      username: faker.internet.userName(),
+      about: faker.lorem.words(),
+      password: faker.internet.password(),
+      email: faker.internet.email()
     }
+    const result = await request
+      .post(`${URI_PREFIX}/users`)
+      .send({ user });
+    expect(result.status).to.be.equal(200);
   })
 });
 
