e7381b3ec1ac0e92126118099e5ea0b438422fea
soufiane <ait.akkache.soufiane@gmail.com>
2018-06-16 16:50:06 +0200

    add configuration for mysql connection client

--- db/database.js
+++ db/database.js
@@ -5,20 +5,38 @@ module.exports = {
     password: 'root',
     database: 'database_test',
     host: 'database',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    dialectOptions: {
+      socketPath: '/var/run/mysqld/mysqld.sock'
+    },
+    define: {
+      paranoid: true
+    }
   },
   test: {
     username: 'root',
     password: 'root',
     database: 'database_test',
     host: 'database',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    dialectOptions: {
+      socketPath: '/var/run/mysqld/mysqld.sock'
+    },
+    define: {
+      paranoid: true
+    }
   },
   production: {
     username: 'root',
     password: 'root',
     database: 'database_production',
     host: '127.0.0.1',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    dialectOptions: {
+      socketPath: '/var/run/mysqld/mysqld.sock'
+    },
+    define: {
+      paranoid: true
+    }
   }
 }
