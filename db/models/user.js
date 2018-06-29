0e382ae4c93fcee237657cd56ef39aec735285c0
soufiane <ait.akkache.soufiane@gmail.com>
2018-06-29 23:53:44 +0200

    comment code of model

--- db/models/user.js
+++ db/models/user.js
@@ -27,66 +27,66 @@ const userScheam = Schema({
   }
 })
 
-export default (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    firstname: {
-      type: DataTypes.STRING
-    },
-    lastname: {
-      type: DataTypes.STRING
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: {
-          args: true,
-          msg: 'cannot be empty'
-        },
-        len: {
-          args: 3,
-          msg: 'must be at least 6 characters in length'
-        }
-      }
-    },
-    about: {
-      type: DataTypes.STRING
-    },
-    password: {
-      type: DataTypes.STRING
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: {
-          args: true,
-          msg: 'email cannot be empty'
-        },
-        isEmail: {
-          args: true,
-          msg: 'retry with another email syntax'
-        }
-      }
-    },
-    status: {
-      type: DataTypes.ENUM,
-      values: ['active', 'inactive'],
-      defaultValue: 'active'
-    }
-  }, {
-    defaultScope: {
-      attributes: { exclude: ['password'] },
-      indexes: [
-        {
-          unique: true,
-          fields: ['email', 'username']
-        }
-      ]
-    }
-  });
-  User.associate = (models) => {
-    // associations can be defined here
-  };
-  return User;
-};
+// export default (sequelize, DataTypes) => {
+//   const User = sequelize.define('User', {
+//     firstname: {
+//       type: DataTypes.STRING
+//     },
+//     lastname: {
+//       type: DataTypes.STRING
+//     },
+//     username: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//       validate: {
+//         notEmpty: {
+//           args: true,
+//           msg: 'cannot be empty'
+//         },
+//         len: {
+//           args: 3,
+//           msg: 'must be at least 6 characters in length'
+//         }
+//       }
+//     },
+//     about: {
+//       type: DataTypes.STRING
+//     },
+//     password: {
+//       type: DataTypes.STRING
+//     },
+//     email: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//       validate: {
+//         notEmpty: {
+//           args: true,
+//           msg: 'email cannot be empty'
+//         },
+//         isEmail: {
+//           args: true,
+//           msg: 'retry with another email syntax'
+//         }
+//       }
+//     },
+//     status: {
+//       type: DataTypes.ENUM,
+//       values: ['active', 'inactive'],
+//       defaultValue: 'active'
+//     }
+//   }, {
+//     defaultScope: {
+//       attributes: { exclude: ['password'] },
+//       indexes: [
+//         {
+//           unique: true,
+//           fields: ['email', 'username']
+//         }
+//       ]
+//     }
+//   });
+//   User.associate = (models) => {
+//     // associations can be defined here
+//   };
+//   return User;
+// };
