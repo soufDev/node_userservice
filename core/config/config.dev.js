08f45b0b087c7ca141598158545a23566d4d9882
soufiane <ait.akkache.soufiane@gmail.com>
2018-07-18 23:57:58 +0200

    add changes

--- core/config/config.dev.js
+++ core/config/config.dev.js
@@ -1,8 +1,8 @@
 import ENV from './env';
 
-console.log(ENV);
+console.log({ ENV });
 const Config = {
-  serverPort: ENV.SERVER_PORT || 9000,
+  serverPort: ENV.SERVER_PORT || 3000,
   dbHost: ENV.DB_HOST || 'localhost',
   dbPassword: ENV.DB_PASSWORD || '',
   dbUser: ENV.DB_USER || 'root',
